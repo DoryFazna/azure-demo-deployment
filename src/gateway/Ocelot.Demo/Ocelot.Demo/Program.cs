9e3803d3c712314161d43b7e0bbe8ad4b77ae0d8
Fazna Fouseen <faznaf@99x.io>
2022-10-12 09:48:15 +0530

    add-scheme

--- src/gateway/Ocelot.Demo/Ocelot.Demo/Program.cs
+++ src/gateway/Ocelot.Demo/Ocelot.Demo/Program.cs
@@ -1,5 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("Ocelot.json");
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
+
+
+app.UseOcelot().Wait();
 
 app.MapGet("/", () => "Hello World!");
 
