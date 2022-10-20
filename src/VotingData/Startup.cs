a29800a43144b2ef471f6b1a93eabc0191da964b
Fazna Fouseen <faznaf@99x.io>
2022-10-20 09:14:20 +0530

    add-healthcheck-ui

--- src/VotingData/Startup.cs
+++ src/VotingData/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Internal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,7 +30,7 @@ namespace VotingData
             var connection = Configuration.GetValue<string>("ConnectionStrings:SqlDbConnection");
             services.AddDbContext<VotingDBContext>(options => options.UseSqlServer(connection));
 
-            services.AddHealthChecks().AddSqlServer("string-here");
+            services.AddHealthChecks().AddSqlServer(Configuration["ConnectionStrings:SqlDbConnection"]);
 
             services.AddHealthChecksUI();
 
@@ -54,10 +57,27 @@ namespace VotingData
             app.UseHttpsRedirection();
             app.UseMvc();
 
+            // app.UseHealthChecks("/health", new HealthCheckOptions
+            // {
+            //     Predicate = _ => true,
+            //     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            // });
+
+            // app.UseHealthChecksUI(setup =>
+            // {
+            //     setup.UIPath = "/healthchecks-ui"; // ui path
+            //     setup.ApiPath = "/health-ui-api"; // this is the internal ui api
+            // });
+
             app.UseHealthChecks("/health");
 
             app.UseHealthChecksUI();
 
+            // app.UseHealthChecksUI(setup =>
+            // {
+            //     setup.UIPath = "/healthchecks"; // ui path
+            //     //setup.ApiPath = "/vote/votedata"; // this is the internal ui api
+            // });
             // We will just create the table on startup if it doesn't exist since this is a demo
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
